15afc6d213a6ab62863cfc8bb6e4978f4ddf5d40
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2024-03-25 22:09:00 +0900

    Define tasks within Hanami::CLI::RakeTasks.register_tasks

--- lib/cerise/rubocop/rake_tasks.rb
+++ lib/cerise/rubocop/rake_tasks.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
-require "rubocop/rake_task"
+Hanami::CLI::RakeTasks.register_tasks do
+  require "rubocop/rake_task"
 
-RuboCop::RakeTask.new
+  RuboCop::RakeTask.new
+end
--- spec/cerise/rubocop_spec.rb
+++ spec/cerise/rubocop_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe Cerise::RuboCop do
   it "defines Rake tasks for running RuboCop" do
     # RuboCop::RakeTask also defines "rubocop:auto_correct" but it is already deprecated.
     expected_tasks = %w[rubocop rubocop:autocorrect rubocop:autocorrect_all]
+
+    Hanami::CLI::RakeTasks.install_tasks
+
     actual_tasks = Rake.application.tasks.map(&:name)
 
     expect(actual_tasks).to include(*expected_tasks)
